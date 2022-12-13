3753d8a71fea12df5f333fe18d2e8c4fec39e554
subzerocatalyst <hypercooledfurry@protonmail.com>
2022-12-13 01:24:26 -0500

    updated map and changed interact system

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	private CharacterController ccont;
+	public GameObject interactIcon;
 	public float speed = 1f;
 	private float sprint = 1f;
+	public float cameraOffset;
 	public Transform cam;
+	public float standingHeight;
+	public float crouchingHeight;
+	public bool crouching;
 	public float xsensitivity;
 	public float ysensitivity;
 	Vector3 camerarot;
@@ -31,10 +36,23 @@ public class PlayerMovement : MonoBehaviour
 			sprint = 1f;
 		}
 		
+		if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl) ){
+			crouching = !crouching;
+			if (crouching){
+				cameraOffset = crouchingHeight/2;
+				ccont.height = crouchingHeight;
+			}else{
+				cameraOffset = standingHeight/2;
+				ccont.height = standingHeight;
+				transform.position = transform.position + new Vector3(0,crouchingHeight/2,0);
+			}
+		}
+		
 		movement.x = Input.GetAxis("Horizontal")*speed*sprint;
 		movement.z = Input.GetAxis("Vertical")*speed*sprint;		
 		
 		movement = transform.TransformDirection(movement);
+		
 		
 		camerarot = new Vector3(Mathf.Clamp(camerarot.x - Input.GetAxis("Mouse Y")*xsensitivity,-90,90),camerarot.y + Input.GetAxis("Mouse X")*ysensitivity,0);
 		
@@ -43,5 +61,19 @@ public class PlayerMovement : MonoBehaviour
 		transform.rotation = Quaternion.Euler(0,camerarot.y,0);
 		
 		ccont.Move(movement*Time.deltaTime);
+		cam.localPosition = new Vector3(0,cameraOffset,0);
+	}
+	
+	void FixedUpdate()
+    {
+        RaycastHit hit;
+		interactIcon.SetActive(false);
+        if (Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, 1.5f)){
+//			Debug.Log("hit gameobject " + hit.transform.gameObject);
+			if (hit.transform.gameObject.CompareTag("interactable")){
+				hit.transform.gameObject.GetComponent<ToggleSwitch>().isSelected = true;
+				interactIcon.SetActive(true);
+			}
+		}
     }
 }
--- Assets/ToggleSwitch.cs
+++ Assets/ToggleSwitch.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ToggleSwitch : CollisionTrigger
+public class ToggleSwitch : MonoBehaviour
 {
 	public AudioClip sfx;
 	public AudioSource src;
 	public Reciever reciever;
 	public float resetDelay;
 	public float timer;
+	public bool isWorking = true;
+	public bool isSelected;
 	
 	private void FixedUpdate(){
 		timer -= Time.deltaTime;
-	}
-	
-	public override void FakeOnTriggerStay(){
-		
+		if(isSelected){
 		if(Input.GetKeyDown(KeyCode.E)){
 			if(timer < 0f){
 				src.PlayOneShot(sfx);
-				if(reciever){
+				if(reciever && isWorking){
 				reciever.Input();
 				}
 				timer = resetDelay;
 			}
+		}
+		isSelected = false;
 		}
 	}
 }
